fb3e20eec3e77d5de385200691f1d72587ee9502
Stephen Brooks <codedev1980@gmail.com>
2023-11-24 10:22:40 -0600

    update

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const app = express();
+const sitemapGenerator = require("sitemap-generator");
+const compression = require("compression");
+const {http} = require("follow-redirects");
+
+http.get('http://localhost:8080/', response => {
+    response.on('data', chunk => {
+        console.log(chunk)
+    })
+})
+// https.get('https://localhost:8080/', response => {
+//     response.on('data', chunk => {
+//         console.log(chunk)
+//     })
+// })
+
+const generator = sitemapGenerator("http://localhost:8080/", {
+    stripQueryString: false
+})
+generator.on('done', () => {
+    //sitemap created
+})
+generator.start();
 
 require("dotenv").config();
 
+app.use(compression());
 app.set("view engine", "ejs");
 
 const port = process.env.PORT;
---
+++ server/controllers/homePageView.js
@@ -0,0 +1,5 @@
+module.xports = (req, res) => {
+    res.render("index", {
+        title: "Web Express I.T. Agency Home Page"
+    })
+}
